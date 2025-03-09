f9b2f828d5213ec23757fc5a0e0a57dfbdb83887
jacribeiro <j.alberto.cribeiro@gmail.com>
2025-03-09 00:25:45 +0000

    Implemented shape and transformations in Piece class. Simplified Player class

--- src/main.py
+++ src/main.py
@@ -1,33 +1,51 @@
 class Player:
     def __init__(self, color: int):
-        self._color: int = color
-        self._buttons: int = 5
-        self._time: int = 0
-        self._is_above: bool = False
-
-    def get_time(self) -> int:
-        return self._time
-    
-    def is_above(self) -> bool:
-        return self._is_above
+        self.color: int = color
+        self.buttons: int = 5
+        self.time: int = 0
+        self.is_above: bool = False
 
 class PatchworkGame:
     def __init__(self):
         self.next_player = 0
 
     def calculate_next_player(player_0: Player, player_1: Player) -> Player:
-        if player_0.get_time() == player_1.get_time():
-            if player_0.is_above():
+        if player_0.time == player_1.time:
+            if player_0.is_above:
                 return player_0
             else: 
                 return player_1
-        elif player_0.get_time() > player_1.get_time():
+        elif player_0.time > player_1.time:
             return player_1
         else:
             return player_0
         
 class Piece:
-    def __init__(self, button_cost, time_cost, gainable_buttons=0):
-        self.button_cost = button_cost
-        self.time_cost = time_cost
-        self.gainable_buttons = gainable_buttons
+    def __init__(self, button_cost: int, time_cost: int, income_buttons: int, shape: list[tuple[int]]):
+        self.button_cost: int = button_cost
+        self.time_cost: int = time_cost
+        self.income_buttons: int = income_buttons
+        self.shape: list[tuple[int]] = self.normalize(shape)
+        self.variations = self.generate_variations(self.shape)
+    
+    @staticmethod
+    def normalize(shape: list[tuple[int]]) -> frozenset:
+        min_x = min(x for x, _ in shape)
+        min_y = min(y for _, y in shape)
+        return frozenset((x - min_x, y - min_y) for x, y in shape)
+    
+    @staticmethod
+    def rotate(shape: list[tuple[int]]) -> frozenset:
+        return Piece.normalize([(y, -x) for x, y in shape])
+    
+    @staticmethod
+    def flip(shape: list[tuple[int]]) -> frozenset:
+        return Piece.normalize([(-x, y) for x, y in shape])
+    
+    def generate_variations(self, shape: list[tuple[int]]):
+        variations = set()
+        for _ in range(4):
+            shape = Piece.rotate(shape)
+            variations.add(shape)
+            variations.add(Piece.flip(shape))
+        return variations
